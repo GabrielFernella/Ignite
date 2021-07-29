0be8ff3ce7bf6818a72e8c469ce2bda1cbfc6e7e
Gabriel Fernella <gabrielfernella07@hotmail.com>
2021-07-29 16:07:42 -0300

    atualizações projeto

---
+++ React/dtmoney/src/App.tsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
+import { GlobalStyle } from './styles/global';
+import { Header } from './components/Header';
+import { Dashboard } from './components/Dashboard';
+import { NewTransactionModal } from './components/NewTransactionModal';
+import { TransactionsProvider } from './hooks/useTransactions';
+
+Modal.setAppElement('#root');
+
+export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+
+  //quando é uma função que interage com botão, adicionamos handle no começo
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
+  return (
+    <TransactionsProvider>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <Dashboard />
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+
+      <GlobalStyle />
+    </TransactionsProvider>
+  );
+}
+
+/*
+ "@typescript-eslint/eslint-plugin": "^4.3.0",
+    "@typescript-eslint/parser": "^4.3.0",
+    "eslint": "6.8.0",
+    "eslint-config-airbnb": "^18.2.0",
+    "eslint-config-prettier": "^6.12.0",
+    "eslint-import-resolver-typescript": "^2.3.0",
+    "eslint-plugin-import": "2.20.1",
+    "eslint-plugin-jsx-a11y": "6.2.3",
+    "eslint-plugin-prettier": "^3.1.4",
+    "eslint-plugin-react": "7.19.0",
+    "eslint-plugin-react-hooks": "2.5.0",
+    "prettier": "^2.1.2"
+
+*/
---
+++ React/ignews/info.md
@@ -0,0 +1,9 @@
+# Pacotes
+1. yarn create next-app ignews
+2. yarn add typescript @types/react @types/node -D
+3. yarn add sass
+4. yarn add react-icons
+5. yarn add stripe
+
+# Commands
+
